28340a0c78661fa7145ca90334821a2b2ec9b37c
2khs21 <104034269+2khs21@users.noreply.github.com>
2025-01-31 17:01:55 +0900

    [Gold II] Title: 친구 네트워크, Time: 456 ms, Memory: 34080 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int T, F;
+map<int,int> parents;
+map<int, int> relationshipCount;
+
+int findParent(int child){
+	int parent = parents[child] == child ? child : parents[child] = findParent(parents[child]);
+	return parent;
+}
+
+void unionGraph(int a, int b){
+	a = findParent(a);
+	b = findParent(b);
+
+	if (a == b)
+		cout << relationshipCount[a] << "\n";
+	else {
+		a < b ? parents[a] = b : parents[b] = a; // bigger to parents
+		relationshipCount[max(a, b)] += relationshipCount[min(a, b)];
+		relationshipCount.erase(min(a, b));
+		cout << relationshipCount[max(a, b)] << "\n";
+	}
+	return ;
+}
+
+void func(){
+	cin >> F;
+	vector<vector<int>> v;
+	v.resize(F);
+	map<string, int> m;
+	int nameIndex = 1;
+	
+	for (int i = 0 ; i < F; i++){
+		string u, v;
+		cin >> u >> v;
+		int ui, vi;
+		ui = m[u] ? m[u] : m[u] = nameIndex++;
+		vi = m[v] ? m[v] : m[v] = nameIndex++;
+		parents[ui] ? : parents[ui] = ui;
+		parents[vi] ? : parents[vi] = vi;
+		if (relationshipCount.find(ui) == relationshipCount.end() && parents[ui] == ui){
+			relationshipCount[ui] = 1;
+		}
+		if (relationshipCount.find(vi) == relationshipCount.end() && parents[vi] == vi){
+			relationshipCount[vi] = 1;
+		}
+		unionGraph(ui, vi);
+	}
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+	cin >> T;
+
+	while (T--){
+		func();
+		parents.clear();
+		relationshipCount.clear();
+	}
+}

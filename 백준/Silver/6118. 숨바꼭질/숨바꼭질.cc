2262501b677b53f14edc3e3dacfe12b23894a74f
2khs21 <104034269+2khs21@users.noreply.github.com>
2025-02-05 05:40:51 +0900

    [Silver I] Title: 숨바꼭질, Time: 36 ms, Memory: 3568 KB -BaekjoonHub

--- 백준/Silver/6118. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/6118. 숨바꼭질/숨바꼭질.cc
@@ -16,37 +16,36 @@ answer : 헛간 번호 / 거리 / 같은 거리 헛간개수
 int N, M;
 vector<int> dis;	
 vector<vector<int>> graph;
-priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+queue<pair<int, int>> Q;
+
 #define W first
 #define NODE second
 #define INF 1e9
-
 int maxDis;
 int dest;
 int cnt;
 
-void dijkstra(){
-	pq.push(make_pair(0, 1));
+void solution(){
+	Q.push({0,1});
 	dis[1] = 0;
-	while(!pq.empty()){
+	while(!Q.empty()){
 		int w, node;
-		tie(w, node) = pq.top();
-		pq.pop();
-		if (w > maxDis) {
+		tie(w, node) = Q.front();
+		if (maxDis < w) {
 			maxDis = w;
 			dest = node;
 			cnt = 1;
-		} else if (w == maxDis){
+		} else if (maxDis == w) {
 			dest = min(dest, node);
 			cnt++;
 		}
-		for (int i = 0 ; i < graph[node].size(); i++){
-			int nw, next;
-			nw = dis[node] + 1;
-			next = graph[node][i];
-			if (dis[next] > nw) {
+		Q.pop();
+		int nw = w + 1;
+		for (int i = 0; i < graph[node].size(); i++){
+			int next = graph[node][i];
+			if (nw < dis[next]){
 				dis[next] = nw;
-				pq.push(make_pair(nw, next));
+				Q.push({nw, next});
 			}
 		}
 	}
@@ -56,7 +55,7 @@ int main(){
 	cin >> N >> M;
 	graph.resize(N + 1);
 	dis.resize(N + 1);
-	for (int i = 1; i <= N; i++){
+	for (int i = 1 ; i <= N; i++){
 		dis[i] = INF;
 	}
 	for (int i = 0 ; i < M; i++){
@@ -66,7 +65,7 @@ int main(){
 		graph[u].push_back(v);
 	}
 
-	dijkstra();
+	solution();
 	
 	cout << dest << " " << maxDis << " " << cnt;
 	

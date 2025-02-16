f24d358145ec4aa665ee8891b50a97b462a03564
2khs21 <104034269+2khs21@users.noreply.github.com>
2025-02-16 12:13:05 +0900

    [level 2] Title: 시소 짝꿍, Time: 7275.47 ms, Memory: 6.44 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <iostream>
 using namespace std;
 
 long long solution(vector<int> weights) {
@@ -9,7 +9,7 @@ long long solution(vector<int> weights) {
     
     sort(weights.begin(), weights.end());
 
-    for (int i = 0 ; i < weights.size() - 1; i++){
+    for (int i = 0 ; i < weights.size() - 1; i++){   // n^2
         for (int j = i + 1; j < weights.size(); j++){
             
             int l = weights[i];
@@ -25,3 +25,45 @@ long long solution(vector<int> weights) {
     
     return answer;
 }
+
+
+
+// long long solution(vector<int> weights) {
+//     long long answer = 0;
+    
+//     sort(weights.begin(), weights.end());
+    
+    
+//     for (int i = 0 ; i < weights.size() - 1; i++){
+//         int l = weights[i];
+        
+//         cout << l  << "\n";
+//         vector<int> targets;
+        
+//         targets.push_back(l);
+//         if (l * 3 % 2 == 0) targets.push_back(l*3/2);
+//         targets.push_back(2*l);
+//         if (l * 4 % 3 == 0) targets.push_back(l*4/3);
+    
+//         cout << "L : " << l <<"\n";
+//         for (auto t : targets){
+            
+//             cout << "t: " << t << " \ncandi : ";
+            
+//             for (auto it = weights.begin() + i + 1; it != weights.end(); it++){
+//                 cout << *it <<" ";
+//             }
+//             cout << "\n";
+//             if (find(weights.begin() + i + 1, weights.end(), t) != weights.end()) {
+//                 answer++;
+//                 cout << "find! ";
+//                 break;
+//             }
+//         }
+//         cout << "\n";
+//     }
+    
+//     return answer;
+// }
+
+

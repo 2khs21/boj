aef8f736bfb1e3474817083fa4e193fe8f21a1c8
2khs21 <104034269+2khs21@users.noreply.github.com>
2025-02-16 18:19:37 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 594.36 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
@@ -13,7 +13,7 @@ int isTrue(long long n) {
     while (n >= 5) {
         long long part = FiveS / 5;
         
-        if (n / part == 2) return 0;
+        if (n / part == 2) return 0;    // 
 
         n = n % part;
         FiveS /= 5;
@@ -28,22 +28,24 @@ int isTrue(long long n) {
 int solution(int n, long long l, long long r) {
     int answer = 0;
     
-    if (l > r) { swap(l, r); }
+    // if (l > r) { swap(l, r); }
     
     for (long long i = l - 1 ; i <= r - 1; i++){
         answer += isTrue(i);
     }
-//     for (long long i = 0; i <= 630 * 5; i++){
-//         // if (i % 5 == 0) cout << " ";
-//         if (i % 25 == 0 ) cout << " ";
-//         if (i % 125 == 0 ) cout << "\n";
-//         if (i % (125 * 5) == 0 ) cout << "\n\n\n";
-//         if (isTrue(i)) cout << "1"; else cout << "0";
-        
-//     }
     
     return answer;
 }
+
+/*
+    k 값을 구할때 , k 보다 큰 5 의 제곱값을 구한다.
+    
+    100 -> 125 
+    ( ) ( ) ( ) ( ) ( )
+
+
+*/
+
 
 
 	

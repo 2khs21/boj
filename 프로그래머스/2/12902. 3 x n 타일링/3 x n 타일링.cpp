f2723d39fd9abb62c79661ac32672a1ac328a1b6
2khs21 <104034269+2khs21@users.noreply.github.com>
2025-02-18 05:33:47 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.03 ms, Memory: 3.79 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+vector<long long> dp;
+
+int solution(int n) {
+    int answer = 0;
+    if (n % 2 == 1) return 0;
+    dp.resize(2501);
+    
+    dp[0] = 1;
+    dp[1] = 3;
+    dp[2] = 11;
+    
+    for (int i = 3; i <= n/2; i++){
+        dp[i] = (dp[i - 1] * 4 - dp[i - 2]);
+        dp[i] = (dp[i] + 1000000007) % 1000000007;
+        
+    }
+    answer = dp[n/2];
+    
+    return answer;
+}

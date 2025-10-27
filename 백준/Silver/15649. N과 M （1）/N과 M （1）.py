56f63eff99b57f828fef98588980c2aa7c04c0a2
2khs21 <104034269+2khs21@users.noreply.github.com>
2025-10-28 05:45:01 +0900

    [Silver III] Title: N과 M (1), Time: 116 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,22 @@
+N, M = list(map(int, input().strip().split()))
+used = [False] * (N + 1)
+sequence = []
+
+
+def dfs(depth):
+	if depth == M:
+		print(' '.join(map(str, sequence)))
+		return
+	
+	for i in range(1, N+1):
+		if not used[i]:
+			used[i] = True
+			sequence.append(i)
+			dfs(depth + 1)
+			sequence.pop()
+			used[i] = False
+
+def solution():
+	dfs(0)
+
+solution()
